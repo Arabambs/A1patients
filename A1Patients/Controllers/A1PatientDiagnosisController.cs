ba5e93c79f44d7f35ec5326c9476dbc1a11bfa0f
Arabams <jeremiah.adesina01@gmail.com>
2020-03-08 18:34:19 -0400

    a projectscr

---
+++ A1Patients/Controllers/A1PatientDiagnosisController.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using A1Patients.Models;
+
+namespace A1Patients.Controllers
+{
+    public class A1PatientDiagnosisController : Controller
+    {
+        private readonly PatientsContext _context;
+
+        public A1PatientDiagnosisController(PatientsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: A1PatientDiagnosis
+        public async Task<IActionResult> Index(string PatientId)
+        {
+            //PatientId has been passed in the URL or QueryString
+            if (PatientId != null)
+            {
+                Response.Cookies.Append("PatientId)", PatientId);
+            }
+
+            //a.	If the PatientId has been passed in the URL or QueryString, save it to a cookie or session variable
+            else if (Request.Query["NewPatientsCode"].Any())
+            {
+                Response.Cookies.Append("PatientId", Request.Query["NewPatientsCode"]);
+                PatientId = Request.Query["NewPatientsCode"];
+            }
+
+            //b.	If the PatientId is not in the QueryString or URL, look for it in a cookie or session variable 
+            else if (Request.Cookies["PatientId"] != null)
+            {
+                PatientId = Request.Cookies["PatientId"].ToString();
+            }
+
+            //… if it’s not there either, go back to the A1Patient listing with a message requesting they select a route.
+            else
+            {
+                TempData["mesage"] = "please select Patient thank you";
+                return RedirectToAction("Index", "A1Patient");
+            }
+
+            var A1patient = _context.Patient.Where(a => a.PatientId == Convert.ToInt32(PatientId)).FirstOrDefault();
+            ViewData["PatientId"] = A1patient.PatientId;
+            ViewData["FirstName"] = A1patient.FirstName;
+            ViewData["LastName"] = A1patient.LastName;      
+
+            var patientsContext = _context.PatientDiagnosis.Include(p => p.Diagnosis).Include(p => p.Patient)
+                .Where(a => a.PatientId == Convert.ToInt32(PatientId))
+                .OrderBy(help1 => help1.Patient.LastName).ThenBy(help1 => help1.Patient.FirstName).ThenByDescending(help1 => help1.PatientDiagnosisId);
+            return View(await patientsContext.ToListAsync());
+        }
+
+        // GET: A1PatientDiagnosis/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patientDiagnosis = await _context.PatientDiagnosis
+                .Include(p => p.Diagnosis)
+                .Include(p => p.Patient)
+                .FirstOrDefaultAsync(m => m.PatientDiagnosisId == id);
+            if (patientDiagnosis == null)
+            {
+                return NotFound();
+            }
+
+            return View(patientDiagnosis);
+        }
+
+        // GET: A1PatientDiagnosis/Create
+        public IActionResult Create()
+        {
+            ViewData["DiagnosisId"] = new SelectList(_context.Diagnosis, "DiagnosisId", "Name");
+            ViewData["PatientId"] = new SelectList(_context.Patient, "PatientId", "FirstName");
+            return View();
+        }
+
+        // POST: A1PatientDiagnosis/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PatientDiagnosisId,PatientId,DiagnosisId,Comments")] PatientDiagnosis patientDiagnosis)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(patientDiagnosis);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DiagnosisId"] = new SelectList(_context.Diagnosis, "DiagnosisId", "Name", patientDiagnosis.DiagnosisId);
+            ViewData["PatientId"] = new SelectList(_context.Patient, "PatientId", "FirstName", patientDiagnosis.PatientId);
+            return View(patientDiagnosis);
+        }
+
+        // GET: A1PatientDiagnosis/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patientDiagnosis = await _context.PatientDiagnosis.FindAsync(id);
+            if (patientDiagnosis == null)
+            {
+                return NotFound();
+            }
+            ViewData["DiagnosisId"] = new SelectList(_context.Diagnosis, "DiagnosisId", "Name", patientDiagnosis.DiagnosisId);
+            ViewData["PatientId"] = new SelectList(_context.Patient, "PatientId", "FirstName", patientDiagnosis.PatientId);
+            return View(patientDiagnosis);
+        }
+
+        // POST: A1PatientDiagnosis/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PatientDiagnosisId,PatientId,DiagnosisId,Comments")] PatientDiagnosis patientDiagnosis)
+        {
+            if (id != patientDiagnosis.PatientDiagnosisId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(patientDiagnosis);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PatientDiagnosisExists(patientDiagnosis.PatientDiagnosisId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DiagnosisId"] = new SelectList(_context.Diagnosis, "DiagnosisId", "Name", patientDiagnosis.DiagnosisId);
+            ViewData["PatientId"] = new SelectList(_context.Patient, "PatientId", "FirstName", patientDiagnosis.PatientId);
+            return View(patientDiagnosis);
+        }
+
+        // GET: A1PatientDiagnosis/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patientDiagnosis = await _context.PatientDiagnosis
+                .Include(p => p.Diagnosis)
+                .Include(p => p.Patient)
+                .FirstOrDefaultAsync(m => m.PatientDiagnosisId == id);
+            if (patientDiagnosis == null)
+            {
+                return NotFound();
+            }
+
+            return View(patientDiagnosis);
+        }
+
+        // POST: A1PatientDiagnosis/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var patientDiagnosis = await _context.PatientDiagnosis.FindAsync(id);
+            _context.PatientDiagnosis.Remove(patientDiagnosis);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PatientDiagnosisExists(int id)
+        {
+            return _context.PatientDiagnosis.Any(e => e.PatientDiagnosisId == id);
+        }
+    }
+}
